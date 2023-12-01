6d7f97c99a23c988c7f62b4f5da3a6b8f5c05bcb
brumePyvault <oghenebrume.akpadaka@queryfier.com>
2023-12-01 19:58:08 +0100

    pug setup front-end begins

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require(`${__dirname}/routes/tourRoutes`);
@@ -9,6 +10,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const nonce = require('crypto').randomBytes(16).toString('base64');
 const appError = require('./utils/appError');
 
 const app = express();
@@ -20,9 +22,27 @@ const limiter = rateLimit({
   legacyHeaders: false // Disable the `X-RateLimit-*` headers
 });
 
+// Set 'views' to the folder where your Pug files are stored
+app.set('views', path.join(__dirname, 'views'));
+
+// Set the view engine to Pug
+app.set('view engine', 'pug');
+
 //1) Middlewares
 //set security headers
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'script-src': ["'self'", 'https://cdn.tailwindcss.com'],
+        'style-src': ["'self'", 'https://cdn.tailwindcss.com', "'unsafe-inline'", 'https://fonts.googleapis.com']
+      }
+    },
+    crossOriginResourcePolicy: false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: false
+  })
+);
 // dev logging
 if (process.env.NODE_ENV == 'development') app.use(morgan('dev'));
 
@@ -43,12 +63,19 @@ app.use(xss());
 app.use(hpp({ whitelist: ['duration', 'difficulty', 'price', 'maxGroupSize', 'ratingsAverage', 'ratingsQuantity'] }));
 
 //serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).render('index', {
+    title: 'Natours | Exciting tours for adventurous people',
+    nonce: nonce
+  });
 });
 
 // limit request from same ip
